0ab1e36b400d453b7c324acf32aca2178a76a2d8
victorviro <victor_sanchez91@hotmail.es>
2021-01-14 22:48:26 +0100

    Added gitignore and requirements files. Added folders to store data. Added constants script.

---
+++ src/constants.py
@@ -0,0 +1,6 @@
+import os
+
+
+PROJECT_PATH = os.getcwd()
+DATASET_PATH = f'{PROJECT_PATH}/data/raw/corpus.csv'
+SENTENCES_EMBEDDINGS_PATH = f'{PROJECT_PATH}/data/processed/sentences_embeddings.csv'
